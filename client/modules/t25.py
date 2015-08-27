71f8b73e0254f07a0bb0bb2ca07a71028b174459
bcorthell <bcorthell@gmail.com>
2015-08-26 20:28:15 -0600

    Added t25 module

---
+++ client/modules/t25.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8-*-
+import random
+import re
+
+WORDS = ["WORKOUT"]
+
+
+def handle(text, mic, profile):
+    """
+        Responds to user-input, typically speech text, by relaying the
+        meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+    """
+    messages = ["Way to go on that workout!",
+                "Why are you cheating on Shaun T?",]
+
+    message = random.choice(messages)
+
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to the meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return bool(re.search(r'\bworkout\b', text, re.IGNORECASE))
+
